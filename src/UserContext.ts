c3192824ba81512655c8cd8703713fbab63e5083
Renegildo <renegildo@proton.me>
2024-02-10 09:43:35 -0300

    fixed top level await error (again)

--- src/UserContext.ts
+++ src/UserContext.ts
@@ -6,9 +6,7 @@ const getUser = async () => {
 	return await account.get();
 }
 
-const user = await getUser();
-
 export const UserContext = createContext<{ user: Models.User<Models.Preferences> | undefined; setUser: React.Dispatch<React.SetStateAction<Models.User<Models.Preferences> | undefined>>; }>({
-	user: user,
+	user: await getUser(),
 	setUser: () => { },
 });
